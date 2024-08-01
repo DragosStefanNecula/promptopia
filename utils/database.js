bbf37cee39a0ce4f3d99e2df11f6e483d19dd9d1
Dragos Stefan Necula <dragosstefannecula@gmail.com>
2024-08-01 18:16:09 +0100

    removed the env URI console log that was there

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,6 @@ let isConnected = false;
 export const connectToDB = async () => {
     mongoose.set('strictQuery', true);
 
-    console.log(process.env.MONGODB_URI)
     if (isConnected) {
         console.log("Mongodb is already connected.")
         return;
